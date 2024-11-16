fdb7d7b153513d073635f616d9d3c8ec262a3eb4
kaan koc <kaankoc01@gmail.com>
2024-11-16 17:40:50 +0300

    Api Dosya İşlemleri

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminFileController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminFileController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            //akışı oluştur
+            var stream = new MemoryStream();
+
+            //dosyayı kopyala
+            await file.CopyToAsync(stream);
+
+            // akıştak dosyayı bytes olarak tutuyoruz
+            var bytes = stream.ToArray();
+
+            ByteArrayContent content = new ByteArrayContent(bytes);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartformdataContent = new MultipartFormDataContent();
+
+            multipartformdataContent.Add(content, "file", file.FileName);
+
+            var httpclient = new HttpClient();
+            var responseMessage = await httpclient.PostAsync("http://localhost:5272/api/FileProcess", multipartformdataContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            //akışı oluştur
+            var stream = new MemoryStream();
+
+            //dosyayı kopyala
+            await file.CopyToAsync(stream);
+
+            // akıştak dosyayı bytes olarak tutuyoruz
+            var bytes = stream.ToArray();
+
+            ByteArrayContent content = new ByteArrayContent(bytes);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartformdataContent = new MultipartFormDataContent();
+
+            multipartformdataContent.Add(content, "file", file.FileName);
+
+            var httpclient = new HttpClient();
+            var responseMessage = await httpclient.PostAsync("http://localhost:5272/api/FileImage",multipartformdataContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return View();
+            }
+            return View();
+        }
+    }
+}
