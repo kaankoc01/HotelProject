02983d501c29b178dc1e8ede514955676c041fb2
kaan koc <kaankoc01@gmail.com>
2024-11-23 20:53:40 +0300

    Sosyal medya takipçi sayıları dashboard a çekildi.

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,9 +1,81 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Crypto.Engines;
+using System.Net.Http.Headers;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/therock"),
+                Headers =
+    {
+        { "x-rapidapi-key", "56da66ad1bmshd8181593cb546adp170170jsn9348eb16959f" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDto = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                ViewBag.v1 = resultInstagramFollowersDto.followers;
+                ViewBag.v2 = resultInstagramFollowersDto.following;
+            }
+
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://twitter32.p.rapidapi.com/getProfile?username=murattyucedag"),
+                Headers =
+    {
+        { "x-rapidapi-key", "56da66ad1bmshd8181593cb546adp170170jsn9348eb16959f" },
+        { "x-rapidapi-host", "twitter32.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ResultTwitterFollowersDto resultTwitterFollowersDto = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
+                ViewBag.v3 = resultTwitterFollowersDto.data.user_info.followers_count;
+                ViewBag.v4 = resultTwitterFollowersDto.data.user_info.friends_count;
+
+            }
+
+
+            var client3 = new HttpClient();
+            var request3 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fu%C4%9Furkaanko%C3%A7%2F&include_skills=false&include_certifications=false&include_publications=false&include_honors=false&include_volunteers=false&include_projects=false&include_patents=false&include_courses=false&include_organizations=false&include_profile_status=false&include_company_public_url=false"),
+                Headers =
+    {
+        { "x-rapidapi-key", "56da66ad1bmshd8181593cb546adp170170jsn9348eb16959f" },
+        { "x-rapidapi-host", "fresh-linkedin-profile-data.p.rapidapi.com" },
+    },
+            };
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode();
+                var body3 = await response3.Content.ReadAsStringAsync();
+                ResultLinkedinFollowersDto resultLinkedinFollowersDto = JsonConvert.DeserializeObject<ResultLinkedinFollowersDto>(body3);
+                ViewBag.v5 = resultLinkedinFollowersDto.data.follower_count;
+
+                Console.WriteLine(body3);
+            }
+
+            return View();
+        }
     }
 }
