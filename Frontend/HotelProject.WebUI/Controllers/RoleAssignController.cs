06a4c4db919ea0dc787e9a99f0ca7284a7275176
kaan koc <kaankoc01@gmail.com>
2024-11-24 18:32:46 +0300

    role işlemleri

---
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssignController.cs
@@ -0,0 +1,22 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoleAssignController : Controller
+    {
+        private readonly UserManager<Appuser> _userManager;
+
+        public RoleAssignController(UserManager<Appuser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+            return View(values);
+        }
+    }
+}
